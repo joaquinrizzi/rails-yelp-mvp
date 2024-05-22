fb1b3a52fed7ae4f8eaefc9657a6d298f950103b
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-05-22 11:57:20 +0200

    --wip-- [skip ci]

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: (0..5).to_a,
+    message: "%{value} must be between 0 and 5" },
+    numericality: { only_integer: true }
+end
